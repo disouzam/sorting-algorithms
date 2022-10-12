8638fbea208c04b872b1822818bb42daff5cf46b
Dickson Souza <disouzam@yahoo.com.br>
2022-10-11 21:49:31 -0300

    Initial project creation]

---
+++ sorting-algorithms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sorting_algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
