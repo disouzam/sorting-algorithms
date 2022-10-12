e09c1f7accbea9244a90e74babd3e92a7587d47b
Dickson Souza <disouzam@yahoo.com.br>
2022-10-12 19:19:26 -0300

    Adding a method to print a random generated array as text file to be reused inside benchmarker

--- console-app/Program.cs
+++ console-app/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.IO;
+using System.Text;
+
 using sorting_algorithms;
 
 namespace consoleapp
@@ -11,6 +14,9 @@ namespace consoleapp
 
             var randomGenerator = new System.Random();
 
+            //var testArray = ArrayFactory.GetRandomArray(randomGenerator.Next());
+            var testArray = ArrayFactory.GetRandomArray(1000, 10);
+            PrintArray(testArray);
             BubbleSort.Sort1(testArray);
 
             Console.WriteLine($"Is {nameof(testArray)} sorted by {nameof(BubbleSort)}.{nameof(BubbleSort.Sort1)}?");
@@ -35,6 +41,30 @@ namespace consoleapp
             Console.WriteLine(SortingAssessment.IsSorted(testArray));
             Console.WriteLine();
 
+        }
+
+        internal static void PrintArray(int[] array)
+        {
+            var filePath = $"{Directory.GetCurrentDirectory()}\\arrayValue.txt";
+
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("[");
+
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                stringBuilder.Append($"{array[i]}");
+
+                if (i < array.Length - 1)
+                {
+                    stringBuilder.Append(",");
+                }
+            }
+
+            stringBuilder.Append("]");
+
+            File.WriteAllText(filePath, stringBuilder.ToString());
         }
     }
 }
