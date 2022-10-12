81819144a8a13f08029e74fa5ef078f0981de922
Dickson Souza <disouzam@yahoo.com.br>
2022-10-12 14:47:11 -0300

    Implementing Sort2 of BubbleSort class - this is the original implementation from Elemar Jr. lecture video

--- sorting-algorithms/BubbleSort.cs
+++ sorting-algorithms/BubbleSort.cs
@@ -15,5 +15,19 @@
                 }
             }
         }
+
+        public static void Sort2(int[] array)
+        {
+            for (var i = array.Length -1; i >= 0; i--)
+            {
+                for (var j = 0; j < i; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        (array[j], array[j+1]) = (array[j+1], array[j]);
+                    }
+                }
+            }
+        }
     }
 }
