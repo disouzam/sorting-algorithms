ea4aa6c598ed6b05e11e6d9df1751d8346915c11
Dickson Souza <disouzam@yahoo.com.br>
2022-10-11 22:52:09 -0300

    Fixing a bug in the swap operation of Bubble Sort - caught by SortingAssessment

--- sorting-algorithms/BubbleSort.cs
+++ sorting-algorithms/BubbleSort.cs
@@ -6,11 +6,11 @@
         {
             for (var i = array.Length -1; i >= 0; i--)
             {
-                for (var j=0; j< i; j++)
+                for (var j = 0; j < i; j++)
                 {
                     if (array[j] > array[j+1])
                     {
-                        (array[j], array[j+1]) = (array[j], array[j+1]);
+                        (array[j], array[j+1]) = (array[j+1], array[j]);
                     }
                 }
             }
