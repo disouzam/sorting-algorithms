eb1166c5868b3a0f133f4f162d2c909bc6642445
Dickson Souza <disouzam@yahoo.com.br>
2022-10-12 15:12:35 -0300

    Adding Sort3 - improved inner loop limits and early break in case of no change in a pass

--- sorting-algorithms/BubbleSort.cs
+++ sorting-algorithms/BubbleSort.cs
@@ -29,5 +29,27 @@
                 }
             }
         }
+
+        public static void Sort3(int[] array)
+        {
+            for (var i = array.Length -1; i >= 0; i--)
+            {
+                var changes = 0;
+
+                for (var j = 0; j < i; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        (array[j], array[j+1]) = (array[j+1], array[j]);
+                        changes++;
+                    }
+                }
+
+                if (changes > 0)
+                {
+                    break;
+                }
+            }
+        }
     }
 }
