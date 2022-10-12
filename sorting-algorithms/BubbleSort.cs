525685ae66f0eac3c0be2b7ee9e9fe1f12cbedba
Dickson Souza <disouzam@yahoo.com.br>
2022-10-11 22:06:29 -0300

    Adding class BubbleSort

---
+++ sorting-algorithms/BubbleSort.cs
@@ -0,0 +1,19 @@
+﻿namespace sorting_algorithms
+{
+    internal class BubbleSort
+    {
+        public static void Sort(int[] array)
+        {
+            for (var i = array.Length -1; i >= 0; i--)
+            {
+                for (var j=0; j< i; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        (array[j], array[j+1]) = (array[j], array[j+1]);
+                    }
+                }
+            }
+        }
+    }
+}
