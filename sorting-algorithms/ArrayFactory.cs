9ae0c38326e5815f882d7a809491e18301a815f7
Dickson Souza <disouzam@yahoo.com.br>
2022-10-11 22:13:06 -0300

    A factory class to create arrays for testing purpose

---
+++ sorting-algorithms/ArrayFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace sorting_algorithms
+{
+    internal class ArrayFactory
+    {
+        public static int[] GetRandomArray(int size)
+        {
+            var array = new int[size];
+            var randomGenerator = new System.Random();
+
+            for (int i = 0; i < size; i++)
+            {
+               array[i] = randomGenerator.Next(int.MaxValue);
+            }
+
+            return array;
+        }
+    }
+}
